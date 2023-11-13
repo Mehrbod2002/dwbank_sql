ab090d98bcfdcb2a6eed948fa133fc48389f061e
mhmd <mhmd@gmail.com>
2023-11-13 11:35:17 +0330

    mhmd

--- dwbank/server/src/dwbank/settings.py
+++ dwbank/server/src/dwbank/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #bool(int(os.environ.get("DEBUG", "1")))
+DEBUG = bool(int(os.environ.get("DEBUG", "1")))
 
-ALLOWED_HOSTS = ['*'] #os.environ.get("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
        
 # Application definition
 
@@ -231,4 +231,3 @@ CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "Asia/Tehran"
 
 CSRF_TRUSTED_ORIGINS=['https://dl.eramglobal.com']
-# CSRF_TRUSTED_ORIGINS=['http://127.0.0.1:8000']
